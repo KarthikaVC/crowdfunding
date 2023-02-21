00dd7050fe5365f31a8a31b4b48105b9bea5f9ab
KarthikaVC <karthikav.rmkcse@gmail.com>
2023-02-21 18:14:24 +0800

    Login Code

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,26 +1,12 @@
 import React from "react";
-import {useState,useEffect} from "react";
-import {useParams} from "react-router-dom";
+
+// Components
+import LoginForm from "../components/LoginForm/LoginForm";
 
 
 function LoginPage(){
-// State
-const [login,setLogin] =useState();
 
-useEffect(() =>{
-    fetch(`${import.meta.env.VITE_API_URL}users/`)
-    .then((results) =>{
-        return results.json();
-    })
-    .then((data)=>{
-        setLogin(data);
-    });
-}, []);
+return (<LoginForm/>);
 
-
-return (
-<div>
-    <h2>This is Login Page</h2>
-</div>);
 }
-export default LoginPage
+export default LoginPage;
